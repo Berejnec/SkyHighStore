0dc5441e8283386297bcdadb10914d49aec281b3
aberejnec <adrian.berejnec@irian.ro>
2022-10-19 16:13:20 +0300

    final fixes

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-not-found',
@@ -7,10 +9,18 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() {
+  constructor(private router: Router,
+              private authService: AuthService) {
   }
 
   ngOnInit(): void {
   }
 
+  navigateToStore() {
+    if(this.authService.isLoggedIn) {
+      this.router.navigate(['products']);
+    } else {
+      this.router.navigate(['sign-in']);
+    }
+  }
 }
