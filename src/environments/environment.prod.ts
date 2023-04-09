de2f8be7caf631cd8c71a847a737c6649f998c44
aberejnec <adrian.berejnec@irian.ro>
2023-04-09 16:02:44 +0300

    trying to deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCoVAHjYX010ZQEQiGb7QKjdbfn-rs9jeY",
+    authDomain: "skyhighstore-50977.firebaseapp.com",
+    projectId: "skyhighstore-50977",
+    storageBucket: "skyhighstore-50977.appspot.com",
+    messagingSenderId: "395731721953",
+    appId: "1:395731721953:web:ec2eeecb74177c8601a1a0",
+    measurementId: "G-BYKKD1Y6DT"
+  }
 };
